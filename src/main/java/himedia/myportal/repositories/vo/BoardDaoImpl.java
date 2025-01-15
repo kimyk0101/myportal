a6ae30da239c472c3c9ca88ca6e1ff5d5890869a
kimyk0101 <kimyk7131@gmail.com>
2025-01-15 12:34:45 +0900

    Board

---
+++ src/main/java/himedia/myportal/repositories/vo/BoardDaoImpl.java
@@ -0,0 +1,46 @@
+package himedia.myportal.repositories.vo;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import himedia.myportal.repositories.BoardDao;
+
+@Repository
+public class BoardDaoImpl implements BoardDao {
+	@Autowired
+	SqlSession sqlSession;
+	
+	@Override
+	public List<BoardVo> selectAll() {
+		List<BoardVo> list = sqlSession.selectList("board.selectAll");
+		return list;
+	}
+
+	@Override
+	public int insert(BoardVo boardVo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public BoardVo getContent(Long no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(BoardVo boardVo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Long no, Long userNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/himedia/myportal/services/BoardServiceImpl.java
@@ -0,0 +1,45 @@
+package himedia.myportal.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import himedia.myportal.repositories.BoardDao;
+import himedia.myportal.repositories.vo.BoardVo;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+	@Autowired
+	BoardDao boardDaoImpl;
+	
+	@Override
+	public List<BoardVo> getList() {
+		return boardDaoImpl.selectAll();
+	}
+
+	@Override
+	public BoardVo getContent(Integer no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean write(BoardVo boardVo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(BoardVo boardVo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteByNoAndUserNo(Integer no, Long userNo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
